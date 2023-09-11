0ac4e1bdd2f1c4d0341bd0e8c0a35427a66d2d6b
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-09-11 16:17:02 +0530

    Refactor error helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,8 +9,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+		app.notFound(w)
 	}
 	files := []string{
 		"./ui/html/base.tmpl.htm", // base must be first in list.
@@ -19,23 +18,19 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.logError.Print(err.Error())
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
+		app.serverError(w, err)
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.logError.Print(err.Error())
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		app.serverError(w, err)
 	}
 }
 
 func (app *application) snipView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
+		app.notFound(w)
 	}
 	fmt.Fprintf(w, "View a specific snip with ID %d...", id)
 }
@@ -43,7 +38,7 @@ func (app *application) snipView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snipCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	if _, err := w.Write([]byte("Create a new snip...")); err != nil {
