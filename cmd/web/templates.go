0c710f34844019168a6f201627d586f102c46a96
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-09-13 00:50:16 +0530

    Custom Template Func (humanDate)

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/piyush-mishra-pm/snip-n-share-go/internal/models"
 )
@@ -24,7 +25,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.htm")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.htm")
 		if err != nil {
 			return nil, err
 		}
@@ -42,4 +43,12 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		cache[name] = ts
 	}
 	return cache, nil
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
