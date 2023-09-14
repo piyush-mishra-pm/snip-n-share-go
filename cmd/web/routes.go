48890e44f5e6b0037b13bad1276d02156b56844f
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-09-14 20:25:05 +0530

    Middleware: Response Headers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,12 +2,12 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snip/view", app.snipView)
 	mux.HandleFunc("/snip/create", app.snipCreate)
-	return mux
+	return secureHeaders(mux)
 }
