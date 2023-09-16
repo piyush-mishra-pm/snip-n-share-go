a800ca93dea92f88938d498ae5a2507a1d8383f0
piyush-mishra-pm <ifs.piyushmishra1991@gmail.com>
2023-09-16 13:40:22 +0530

    Refactor input validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,18 +5,17 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/piyush-mishra-pm/snip-n-share-go/internal/models"
+	"github.com/piyush-mishra-pm/snip-n-share-go/internal/validator"
 )
 
 type snipCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -70,29 +69,18 @@ func (app *application) snipCreatePost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	form := snipCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: make(map[string]string),
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Content, 4000), "content", "This field cannot be more than 4000 characters long")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Content) > 5000 {
-		form.FieldErrors["content"] = "This field cannot be more than 5000 characters long"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.htm", data)
--- go.mod
+++ go.mod
@@ -2,9 +2,8 @@ module github.com/piyush-mishra-pm/snip-n-share-go
 
 go 1.21.0
 
-require github.com/go-sql-driver/mysql v1.7.1
-
 require (
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/justinas/alice v1.2.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
 )
